45132495e90c042f3eed9ddd0c810325e331189c
Bailey M <bailey-middendorf@bethel.edu>
2020-05-28 10:38:26 -0500

    add in basic context_processor and before_request

--- shuttle/__init__.py
+++ shuttle/__init__.py
@@ -15,3 +15,29 @@ if app.config['ENVIRON'] == 'prod' and app.config['SENTRY_URL']:
 from shuttle.views import View
 
 View.register(app)
+
+
+# This makes these variables open to use everywhere
+@app.context_processor
+def utility_processor():
+    to_return = {}
+    to_return.update({
+        'now': datetime.now(),
+        'alert': sc().get_alert(),
+    })
+
+    return to_return
+
+
+@app.before_request
+def before_request():
+    if '/static/' in request.path \
+            or '/assets/' in request.path \
+            or '/cron/' in request.path \
+            or '/no-cas/' in request.path:
+
+        if 'ALERT' not in flask_session.keys():
+            flask_session['ALERT'] = []
+    else:
+        if 'ALERT' not in flask_session.keys():
+            flask_session['ALERT'] = []
