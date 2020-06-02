31d59b90bf1ed618b128992b883e03883702f628
trv48226 <58450961+trv48226@users.noreply.github.com>
2020-06-02 11:53:22 -0500

    Update paths

--- shuttle/schedules/__init__.py
+++ shuttle/schedules/__init__.py
@@ -6,3 +6,7 @@ class SchedulesView(FlaskView):
     @route('/shuttle-stats')
     def stats(self):
         return render_template('/schedules/shuttle_stats.html')
+
+    @route('/request-shuttle')
+    def request(self):
+        return render_template('/schedules/request_shuttle.html')
