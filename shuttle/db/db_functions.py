ba6aca4eb09bd0b84d6078c4587a6b19d50fc6d4
Bailey M <bailey-middendorf@bethel.edu>
2020-06-08 08:47:11 -0500

    add in some db helper code

--- shuttle/db/db_functions.py
+++ shuttle/db/db_functions.py
@@ -31,6 +31,52 @@ def get_results(result, label=""):
     return ret
 
 
+# This method takes in a query and runs it. "query_type" will be either "read" or "write."
+# "write" will not call "get_formated_results" and just return a string.
+# "read" will return formatted results of the query.
+def query(sql, query_type):
+    conn = engine.connect()
+    transaction = conn.begin()
+    results = conn.execute(sql)  # This autocommits in most cases, but for our "merge" statements we need to commit
+    transaction.commit()
+    if query_type == 'read':
+        results = get_formatted_results(results)
+    conn.close()
+    return results
+
+
+def get_formatted_results(query_results):
+    columns = query_results.keys()  # This gets the column names of the results
+    results = {}
+    for i, row in enumerate(query_results):
+        row_dict = {}
+        j = 0
+        for column in columns:
+            row_dict[column] = row[j]
+            j += 1
+        results[int(i)] = row_dict
+    return results
+
+
+def get_table_columns(table):
+    ret = []
+    # 'table' needs to be uppercase to run correctly
+    sql = "SELECT column_name FROM USER_TAB_COLUMNS WHERE table_name = '{0}'".format(table.upper())
+    results = query(sql, 'read')
+    for key, columns in results.items():
+        ret.append(columns['column_name'].lower())
+    return ret
+
+
+def check_if_table_exists(table):
+    # 'table' needs to be uppercase to run correctly
+    sql = "SELECT table_name FROM user_tables where table_name = '{0}'".format(table.upper())
+    results = query(sql, 'read')
+    if results:
+        return True
+    return False
+
+
 def mybethel_roles(username):
     conn = None
     try:
