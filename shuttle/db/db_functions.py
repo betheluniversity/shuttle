739f7646a7e6549d296788300f7012b5ebe86756
Trevor Vollendorf <58450961+trv48226@users.noreply.github.com>
2020-06-10 13:40:14 -0500

    Adds Deletion of table in DB
    Deletes table contents before inserting new table values

--- shuttle/db/db_functions.py
+++ shuttle/db/db_functions.py
@@ -115,30 +115,33 @@ def portal_common_profile(username):
 
 
 def commit_schedule_to_db(table):
-    # sql = "DELETE FROM SHUTTLE_SCHEDULE WHERE LOCATION = 'CLC'"
-    # query(sql, 'write')
+    sql = "DELETE FROM SHUTTLE_SCHEDULE"
+    query(sql, 'write')
 
     for i in range(len(table)):
         location = ""
         for j in range(len(table[i])):
-            arrival_time = 0
-            if table[i][j].lower() == 'clc' or table[i][j].lower() == 'anderson' or table[i][j].lower() == 'pine tree' \
-                        or table[i][j].lower() == 'kresge' or table[i][j].lower() == 'scandia' or table[i][j].lower() == 'north':
-                        location = table[i][j].upper()
-            elif table[i][j] == '-' or table[i][j] == 'DROP':
+            try:
+                arrival_time = 0
+                if table[i][j].lower() == 'clc' or table[i][j].lower() == 'anderson' or table[i][j].lower() == 'pine tree' \
+                            or table[i][j].lower() == 'kresge' or table[i][j].lower() == 'scandia' or table[i][j].lower() == 'north':
+                    location = table[i][j].upper()
+                elif table[i][j] == '-' or table[i][j] == 'DROP':
                     arrival_time = '01-AUG-00 01.00.00.000000000 AM'
-            elif table[i][j].split(':') is not None:
+                elif table[i][j].split(':') is not None:
                     split_time = table[i][j].split(':')
-                    if int(split_time[0]) == 12 or int(split_time[0]) >= 1 and int(split_time[0]) < 6:
+                    if int(split_time[0]) == 12 or 1 <= int(split_time[0]) < 6:
                             joined_time = '.'.join(split_time)
                             arrival_time = '01-SEP-00 ' + joined_time + '.00.000000000 PM'
                     else:
-                        joined_time = '.'.join(split_time)
-                        arrival_time = '01-SEP-00 ' + joined_time + '.00.000000000 AM'
-            else:
+                            joined_time = '.'.join(split_time)
+                            arrival_time = '01-SEP-00 ' + joined_time + '.00.000000000 AM'
+                else:
+                    return "data in calendar does not match specified format"
+                if j is not 0:
+                    sql = "INSERT INTO SHUTTLE_SCHEDULE (LOCATION,ARRIVAL_TIME) VALUES (" + "\'" + location + "\',\'" + arrival_time + "\')"
+                    query(sql, 'write')
+            except:
                 return "data in calendar does not match specified format"
-            if j is not 0:
-                sql = "INSERT INTO SHUTTLE_SCHEDULE (LOCATION,ARRIVAL_TIME) VALUES (" + "\'" + location + "\',\'" + arrival_time + "\')"
-                query(sql, 'write')
 
     return "success"
