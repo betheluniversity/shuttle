858aa292fd21367d5c60d9d52dec5692d21e5165
Trevor Vollendorf <58450961+trv48226@users.noreply.github.com>
2020-06-19 14:17:41 -0500

    Reformats driver logs
    The date can now be selected to show the exact log the user is looking for. Error when no log is selected but the submit button is pressed. Dates no longer include 00:00:00 on screen. The formatting for load_logs is strange but that's how pycharm wanted it

--- shuttle/schedules/__init__.py
+++ shuttle/schedules/__init__.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, request
 from flask_classy import FlaskView, route
 from shuttle.db import db_functions as db
 from shuttle.shuttle_controller import ShuttleController
+
+shuttle_logs = {}
 
 
 class SchedulesView(FlaskView):
@@ -30,13 +32,25 @@ class SchedulesView(FlaskView):
     @route('/driver-logs')
     def logs(self):
         self.sc.check_roles_and_route(['Administrator'])
+        global shuttle_logs
         shuttle_logs = db.get_shuttle_logs()
         date = 0
         date_list = []
         for i in range(len(shuttle_logs)):
             real_name = db.username_search(shuttle_logs[i]['username'])
             shuttle_logs[i]['name'] = real_name[0]['firstName'] + " " + real_name[0]['lastName']
+            shuttle_logs[i]['log_date'] = str(shuttle_logs[i]['log_date']).split(" ")[0]
             if date != shuttle_logs[i]['log_date']:
                 date = shuttle_logs[i]['log_date']
                 date_list.append(date)
         return render_template('schedules/driver_logs.html', **locals())
+
+    @route('/shuttle_logs', methods=['GET', 'POST'])
+    def shuttle_logs(self):
+        json_data = request.get_json()
+        date = json_data['date']
+        logs = shuttle_logs
+        if date == '':
+            self.sc.set_alert('danger', 'Please select a log')
+            return "no log"
+        return render_template('schedules/load_logs.html', **locals())
