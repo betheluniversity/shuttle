aa45b01dcbfdf694bd4054a151822f36f03bfc9c
bea73693 <56211155+bea73693@users.noreply.github.com>
2020-08-26 15:13:15 -0500

    Add driver logs export to csv

--- shuttle/driver_logs/__init__.py
+++ shuttle/driver_logs/__init__.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from io import StringIO
+import csv
 
 # Packages
 from flask import render_template
-from flask_classy import FlaskView, route, request
+from flask_classy import FlaskView, route, request, Response
 
 # Local
 from shuttle.schedules.shuttle_schedules_controller import ScheduleController
@@ -36,3 +38,44 @@ class DriverLogsView(FlaskView):
         completed_requests = selected_logs[2]
         deleted_requests = selected_logs[3]
         return render_template('driver_logs/load_logs.html', **locals())
+
+
+    @route('/download-logs/<date>/<name_sort>', methods=['GET', 'POST'])
+    def export_logs(self, date, name_sort):
+        selected_logs = self.ssc.grab_selected_logs(date, name_sort)
+        current_date = datetime.now().strftime('%m/%d/%Y')
+        date = datetime.strptime(date, '%b-%d-%Y').strftime('%m/%d/%y')
+        csv_name = 'driver_logs_' + date
+        log_type = ['Shuttle Logs', 'Break Logs', 'Completed Logs', 'Deleted Logs']
+
+        def generate():
+            # sets up a buffer and start file
+            data = StringIO()
+            writer = csv.writer(data)
+            new_line = []
+            writer.writerow([csv_name, 'Exported on: ' + current_date])
+            writer.writerow(new_line)
+
+            for i in range(len(selected_logs)):
+                for j in range(len(selected_logs[i])):
+                    # grabs a log, adds its data if its not empty
+                    log_dict = selected_logs[i][j]
+                    if log_dict:
+                        if 'arrival_time' in log_dict:
+                            del log_dict['arrival_time']
+
+                        dict_writer = csv.DictWriter(data, fieldnames=log_dict.keys())
+                        if j == 0:
+                            writer.writerow([log_type[i]])
+                            dict_writer.writeheader()
+                        dict_writer.writerow(log_dict)
+                        yield data.getvalue()
+                        data.seek(0)
+                        data.truncate(0)
+                writer.writerow(new_line)
+            data.close()
+
+        file_headers = {"Content-disposition": "attachment; filename=" + csv_name + '.csv'}
+        return Response(generate(),
+                        mimetype='text/csv',
+                        headers=file_headers)
